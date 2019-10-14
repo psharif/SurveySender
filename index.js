dc22cf55c0e0ea9491c2c7b9ddfc702e376ca499
psharif <psharif2788@gmail.com>
2019-10-13 21:15:38 -0700

    Updated passport and mongoDB initialization files

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const { mongoURI, cookieKey } = require("./config/keys.js");
+/// Uses Models Folder with Users File
+require("./models/User.js");
+/// Uses Passport File
 require("./services/passport.js");
+
+mongoose.connect(mongoURI);
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes.js")(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const mongoose = require("mongoose");
 const { googleClientID, googleClientSecret } = require("../config/keys.js");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,8 +23,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("Access Token " + accessToken + "\n");
-      console.log(profile);
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          /// Do nothing becuase this user already exists.
+          done(null, existingUser);
+        } else {
+          /// Create a new user because they are not in the database
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
